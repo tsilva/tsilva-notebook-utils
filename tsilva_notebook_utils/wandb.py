f295585a9e5b42f04ae108af89397a6a4cb19803
tsilva <eng.tiago.silva@gmail.com>
2025-04-04 12:38:50 +0100

    feat: add function to render W&B run iframe in Jupyter notebooks

--- tsilva_notebook_utils/wandb.py
+++ tsilva_notebook_utils/wandb.py
@@ -13,3 +13,15 @@ def login(api_key: str, project_id: str) -> str:
     # Return the run URL
     run_url = run.get_url()
     return run_url
+
+def render_run_iframe():
+    import wandb
+    from IPython.display import HTML
+    from IPython.display import display
+
+    run_url = wandb.run.get_url()
+    iframe_code = f"""
+    <iframe src="{run_url}" width="100%" height="1200px"></iframe>
+    """
+
+    display(HTML(iframe_code))
