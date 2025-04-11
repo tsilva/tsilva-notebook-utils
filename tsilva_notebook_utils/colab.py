9763b31ea608302914c128d6c9cde626980b9893
tsilva <eng.tiago.silva@gmail.com>
2025-04-11 17:08:10 +0100

    feat: added notify_and_disconnect_after_timeout and load_secrets_into_env to colab utils

--- tsilva_notebook_utils/colab.py
+++ tsilva_notebook_utils/colab.py
@@ -1,7 +1,10 @@
-def disconnect_after_timeout(timeout_seconds=300):    
+def disconnect_after_timeout(timeout_seconds=None):    
     import time
-    from google.colab import runtime
     from tqdm import tqdm
+    from google.colab import runtime
+
+      # Default to 5 minutes if not specified
+    if timeout_seconds is None: timeout_seconds = 60 * 5
 
     print(f"Starting idle timeout check. Will disconnect after {timeout_seconds} seconds of no interruption...")
 
@@ -16,3 +19,36 @@ def disconnect_after_timeout(timeout_seconds=300):
 
     print("Idle timeout reached. Disconnecting runtime...")
     runtime.unassign()
+
+def notify_and_disconnect_after_timeout(message="Notebook execution finished!", timeout_seconds=None):  
+    import os
+    from .popdesk import notify
+    
+    notebook_id = os.getenv("NOTEBOOK_ID"); assert notebook_id is not None, "NOTEBOOK_ID environment variable is not set"
+    notification_url = os.getenv("NOTIFICATION_URL"); assert notification_url is not None, "NOTIFICATION_URL environment variable is not set"
+    notification_auth_token = os.getenv("NOTIFICATION_AUTH_TOKEN"); assert notification_auth_token is not None, "NOTIFICATION_AUTH_TOKEN environment variable is not set"
+    
+    # Send notification
+    notify(
+        notification_url,
+        notification_auth_token,
+        notebook_id,
+        message
+    )
+
+    # Disconnect runtime after timeout
+    disconnect_after_timeout(timeout_seconds=timeout_seconds)
+
+def load_secrets_into_env(keys):
+    import os
+    from google.colab import userdata
+
+    for key in keys:
+        # Load the secret from userdata
+        value = userdata.get(key)
+        
+        # Assert that the value is available
+        assert value, f"Key {key} not found in userdata"
+
+        # Set the secret in the environment
+        os.environ[key] = value
