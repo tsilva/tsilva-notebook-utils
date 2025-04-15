900e6bcfc4b9f0962996ecf63cf439264991e12b
tsilva <eng.tiago.silva@gmail.com>
2025-04-15 12:20:12 +0100

    feat: added set_seed function

--- tsilva_notebook_utils/colab.py
+++ tsilva_notebook_utils/colab.py
@@ -69,3 +69,18 @@ def notebook_id_from_title():
     text = metadata['ipynb']['cells'][0]['source'][0]
     slug = _slugify(text)
     return slug
+
+
+def set_seed(seed: int):
+    import random
+    import numpy as np
+    import torch
+    
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
