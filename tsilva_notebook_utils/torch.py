a0d0aa216d64e8ab4c9aaa80d45b35b23c530fac
tsilva <eng.tiago.silva@gmail.com>
2025-04-15 14:34:05 +0100

    refactor: moved gpu utils to torch

--- tsilva_notebook_utils/torch.py
+++ tsilva_notebook_utils/torch.py
@@ -1,3 +1,24 @@
+def get_current_device():
+    import torch
+    return torch.cuda.current_device() if torch.cuda.is_available() else torch.device("cpu")
+
+def get_gpu_stats():
+    import torch
+    assert torch.cuda.is_available(), "CUDA is not available. Running on CPU."
+
+    device = get_current_device()
+    total_memory = torch.cuda.get_device_properties(device).total_memory / 1024**3
+    allocated_memory = torch.cuda.memory_allocated(device) / 1024**3
+    cached_memory = torch.cuda.memory_reserved(device) / 1024**3
+    free_memory = total_memory - allocated_memory
+
+    return {
+        "total_memory_gb": total_memory,
+        "allocated_memory_gb": allocated_memory,
+        "cached_memory_gb": cached_memory,
+        "free_memory_gb": free_memory
+    }
+
 def inspect_tensor_dataset(dataset, show_samples=3):
     """
     Returns a dictionary describing a TensorDataset.
