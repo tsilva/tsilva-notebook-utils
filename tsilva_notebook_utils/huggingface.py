5ee77cc87b662ded9aea2c8c818635aa1d5b2888
Tiago Silva <eng.tiago.silva@gmail.com>
2025-04-25 12:06:59 +0100

    feat: bump version to 0.0.48; add functions to create and push models to Hugging Face Hub

--- tsilva_notebook_utils/huggingface.py
+++ tsilva_notebook_utils/huggingface.py
@@ -88,3 +88,35 @@ def process_images(images, mode="color", quantize_colors=None, scale=1.0, crop_p
             processed.append(image_np)
 
     return processed
+
+def hf_create_or_get_repo(repo_name: str, private: bool = True) -> str:
+    from huggingface_hub import create_repo, HfApi
+    from huggingface_hub.utils import RepositoryNotFoundError
+
+    api = HfApi()
+    
+    try:
+        # Check if repo exists
+        _ = api.repo_info(repo_name)
+        print(f"Repository '{repo_name}' already exists.")
+        return f"https://huggingface.co/{repo_name}"
+    except RepositoryNotFoundError:
+        # Create if not exists
+        repo_url = create_repo(repo_name, private=private)
+        print(f"Repository '{repo_name}' created.")
+        return repo_url
+
+def hf_push_model_to_hub(repo_id: str, model, model_file_name="model.pt", private: bool = True):
+    import torch
+    from huggingface_hub import upload_file
+    
+    hf_create_or_get_repo(repo_id, private=private)
+
+    torch.save(model.state_dict(), model_file_name)
+
+    upload_file(
+        path_or_fileobj=model_file_name,
+        path_in_repo=model_file_name,
+        repo_id=repo_id,
+        repo_type="model"
+    )
