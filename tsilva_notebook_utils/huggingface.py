e1d1d217a93288432e7a7fc0d5d3e520e4e5b7c4
Tiago Silva <eng.tiago.silva@gmail.com>
2025-04-29 13:46:24 +0100

    Update huggingface.py

--- tsilva_notebook_utils/huggingface.py
+++ tsilva_notebook_utils/huggingface.py
@@ -4,7 +4,7 @@ def huggingface_login(token: str):
     return whoami()
 
 def print_trainer_summary(result):
-    from .gpu import get_gpu_stats
+    from .torch import get_gpu_stats
     print(f"Time: {result.metrics['train_runtime']:.2f}")
     print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
     for key, value in get_gpu_stats().items(): print(f"{key}: {value:.4f}")
