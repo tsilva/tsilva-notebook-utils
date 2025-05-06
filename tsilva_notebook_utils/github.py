a96d7a9396b46640ed32b1bc127709d7ecb7e85c
tsilva <eng.tiago.silva@gmail.com>
2025-05-06 15:20:30 +0100

    feat: update version to 0.0.63 in pyproject.toml and modify github_get_file_contents function parameters for consistency

--- tsilva_notebook_utils/github.py
+++ tsilva_notebook_utils/github.py
@@ -1,15 +1,15 @@
-def github_get_file_contents(repo, file_path, branch="main", owner=None, token=None):
+def github_get_file_contents(repo, file_path, branch="main", username=None, token=None):
     import os
     import requests
     import base64
 
-    if owner is None: owner = os.getenv("GITHUB_OWNER")
-    if not owner: raise ValueError("GITHUB_OWNER environment variable not set")
+    if username is None: owner = os.getenv("GITHUB_USERNAME")
+    if not username: raise ValueError("GITHUB_USERNAME environment variable not set")
 
     if token is None: token = os.getenv("GITHUB_TOKEN")
     if not token: raise ValueError("GITHUB_TOKEN environment variable not set")
 
-    api_url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}?ref={branch}"
+    api_url = f"https://api.github.com/repos/{username}/{repo}/contents/{file_path}?ref={branch}"
     headers = {
         "Authorization": f"token {token}",
         "Accept": "application/vnd.github.v3+json"
