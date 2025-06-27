604b910929d1e75cacf3e08f5968580ed88192e0
tsilva <eng.tiago.silva@gmail.com>
2025-06-27 18:55:29 +0100

    fix: correct vec_env_cls assignment in build_env function for clarity

--- tsilva_notebook_utils/gymnasium.py
+++ tsilva_notebook_utils/gymnasium.py
@@ -517,8 +517,8 @@ def build_env(env_id, n_envs=1, seed=None, norm_obs=False, norm_reward=False):
     import multiprocessing
     from stable_baselines3.common.env_util import make_vec_env
     from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv, SubprocVecEnv
-    vec_env_cls = SubprocVecEnv if n_envs > 1 else DummyVecEnv
     if n_envs == 'auto': n_envs = multiprocessing.cpu_count()
+    vec_env_cls = SubprocVecEnv if n_envs > 1 else DummyVecEnv
     env = make_vec_env(env_id, n_envs=n_envs, seed=seed, vec_env_cls=vec_env_cls)
     if norm_obs or norm_reward: env = VecNormalize(env, norm_obs=norm_obs, norm_reward=norm_reward)
     return env
