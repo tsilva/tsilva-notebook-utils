db6ff56afd25695401576e9abb6710af4feac6eb
Tiago Silva <tsilva@tynker.com>
2025-07-14 12:35:07 +0100

    Bump version: 0.0.114 → 0.0.115

--- tsilva_notebook_utils/__init__.py
+++ tsilva_notebook_utils/__init__.py
@@ -2,7 +2,7 @@
 tsilva_notebook_utils - Utility functions for Jupyter/Colab notebooks
 """
 
-__version__ = "0.0.114"
+__version__ = "0.0.115"
 
 # Modules are available for import as: import tsilva_notebook_utils.gymnasium
 # We don't import all modules by default to avoid heavy dependencies
